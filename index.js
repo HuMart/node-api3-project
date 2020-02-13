162e9e79c40945acc798be042e6f861d6acedaf7
HuMart <manya01@protonmail.com>
2020-02-13 12:07:13 -0500

    set up server to work dynamic with dotenv

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // code away!
+require('dotenv').config();
 const server = require('./server.js');
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 server.listen(port, () => console.log(`\n *** api running on port:${port} ***`))
